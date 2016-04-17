0da337f00505821c88c1346f27980531dca6ef6b
rodolfolottin <rodolfolottin1@gmail.com>
2016-04-16 21:01:25 -0300

    Cria novo __init__ Nodo

--- 8-puzzle-solver/Nodo.py
+++ 8-puzzle-solver/Nodo.py
@@ -2,11 +2,13 @@
 
 class Nodo(object):
 
-    def __init__(self, estadoTabuleiro):
+    heuristicaCaminho = self.pesoHeuristica + self.profundidade
+
+    def __init__(self, estadoTabuleiro, pesoHeuristica, profundidade, nodoPai):
         self.estadoTabuleiro = estadoTabuleiro
-        self.pesoHeuristica = 0
-        self.profundidade = 0
-        self.nodoPai = None
+        self.pesoHeuristica = pesoHeuristica
+        self.profundidade = profundidade
+        self.nodoPai = nodoPai
 
     def __repr__(self):
         return '{}: {} {} {} {}'.format(self.__class__.__name__,
@@ -14,14 +16,3 @@ class Nodo(object):
                                   self.pesoHeuristica,
                                   self.profundidade,
                                   self.nodoPai)
-
-    # Docstrings: Função utilizada para imprimir um espaço de estado do quebra cabeça de 8 peças
-    def exibirEstadoDoNodo(self):
-        j = 1
-
-        tabuleiro = self.estadoTabuleiro
-
-        for i in (1, 2, 3):
-            print('- - - - - - - - - -')
-            print('|  ' + str(tabuleiro['a' + str(i) + str(j)]) + '  |  ' + str(tabuleiro['a' + str(i) + str(j + 1)]) + '  |  ' + str(tabuleiro['a' + str(i) + str(j + 2)]) + '  |')
-        print('- - - - - - - - - -')
