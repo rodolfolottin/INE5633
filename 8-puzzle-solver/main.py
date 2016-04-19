cdac4690bbb58a6e29b279f192a3356e97a7a2a1
rodolfolottin <rodolfolottin1@gmail.com>
2016-04-19 10:47:35 -0300

    Cria novos testes main.py

--- 8-puzzle-solver/main.py
+++ 8-puzzle-solver/main.py
@@ -3,10 +3,18 @@ from Heuristica import Heuristica
 from Utils import Utils
 from EightPuzzleSolver import EightPuzzleSolver
 
-estadoTabuleiro = {'a11': 2, 'a12': ' ', 'a13': 3, 
-				   'a21': 4, 'a22': 8, 'a23': 6,
-				   'a31': 1, 'a32': 5, 'a33': 7}
+estadoTabuleiro = {'a11': 2, 'a12': 3, 'a13': 6,
+				   'a21': 4, 'a22': 5, 'a23': 8,
+				   'a31': ' ', 'a32': 1, 'a33': 7}
 
-nodoInicial = Nodo(estadoTabuleiro, 0, 0, None)
+estadoTabuleiro2 = {'a11': 6, 'a12': 5, 'a13': 3,
+                   'a21': 4, 'a22': 7, 'a23': 1,
+                   'a31': 2, 'a32': 8, 'a33': ' '}
+
+estadoTabuleiro3 = {'a11': 3, 'a12': 8, 'a13': 6,
+                   'a21': 1, 'a22': ' ', 'a23': 5,
+                   'a31': 2, 'a32': 7, 'a33': 4}
+
+nodoInicial = Nodo(estadoTabuleiro3, 0, 0, None)
 EightPuzzleSolver = EightPuzzleSolver()
 EightPuzzleSolver.AStarAlgorithm(nodoInicial, True)
