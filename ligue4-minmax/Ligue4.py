2c1bb48bbaea20d273dcb6d8dccbbbd5feece33e
rodolfolottin <rodolfolottin1@gmail.com>
2016-05-01 01:51:49 -0300

    Add set jogadorDaVez, atualizaJogada, descobrePosicoes

--- ligue4-minmax/Ligue4.py
+++ ligue4-minmax/Ligue4.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import argparse
+from random import randint
 
 
 class Ligue4(object):
@@ -8,7 +9,11 @@ class Ligue4(object):
         self._modo = modo
         self._tabuleiro = [[None for x in xrange(6)] for y in xrange(7)]
         self._profundidade = self._profundidadeModo(modo)
-        self._jogadorDaVez
+        self._posicoesDisponiveis = None
+        self._jogadorDaVez = None
+
+        if randint(0, 1) == 0:
+            self._jogadorDaVez = 'Computador'
 
     def _profundidadeModo(self, dificuldade):
         if dificuldade == 'dificil':
@@ -18,8 +23,43 @@ class Ligue4(object):
         else:
             self._profundidade = 2
 
+    # ToDo implementar função
+    def _descobrePosicoesDisponiveisTabuleiro(self):
+        for linha in self._tabuleiro:
+            for coluna in linha:
+                print coluna
+                self._posicoesDisponiveis.append(coluna.count(not None))
+
+    def _atualizaJogada(self):
+        self._descobrePosicoesDisponiveis()
+        # se jogadorDaVez for Computador, passa a ser Player, se não passa a ser Computador
+        if self._jogadorDaVez == 'Computador':
+            self._jogadorDaVez = 'Player'
+        else:
+            self._jogadorDaVez = 'Computador'
+
     def run(self):
-        pass
+        self._descobrePosicoesDisponiveisTabuleiro()
+
+        while True:
+            if self._jogadorDaVez == 'Jogador':
+                input_msg = 'Em que posicao você deseja jogar? ' + str(self._posicoesDisponiveis)
+
+                try:
+                    jogada = int(raw_input(input_msg))
+                    if jogada not in self._posicoesDisponiveis:
+                        print 'A posição fornecida não é uma das posições disponíveis'
+                except ValueError:
+                    print 'A posição fornecida não é um número fornecido não é um número'
+                    jogada = None
+                    continue
+
+                # Acabou jogada, passa vez
+                self._atualizaJogo(self._jogadorDaVez)
+            else:
+                'Print Rodolfo'
+                # Acabou jogada, passa vez
+                self._atualizaJogo(self._jogadorDaVez)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Emula o jogo Ligue4 (Jogador vs Computador). Foi utilizado o algoritmo Minimax com poda alfa e beta para implementação das jogadas do computador.')
