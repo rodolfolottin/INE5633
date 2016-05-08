54dc16dbf25225d686ac7c358bd1a7daf4dd08a7
rodolfolottin <rodolfolottin1@gmail.com>
2016-05-08 12:59:40 -0300

    Add clear os system e import classe minmax

--- ligue4-minmax/Ligue4.py
+++ ligue4-minmax/Ligue4.py
@@ -1,30 +1,29 @@
 # coding: utf-8
 from Utils import Utils
 from Peca import Peca
-import argparse
+from Minimax import Minimax
 from random import randint
+import argparse
+import os
 
 
 class Ligue4(object):
 
-    def __init__(self, modo, nomeJogador):
+    def __init__(self, nomeJogador):
         self._tabuleiro = [[Peca.VAZIA for x in xrange(6)] for y in xrange(7)]
-        self._posicoesDisponiveis = Utils.descobrePosicoesDisponiveisTabuleiro(self._tabuleiro)
         self._jogador = nomeJogador
+        self._posicoesDisponiveis = []
+        self._minMax = Minimax(5)
 
         if randint(0, 2) == 0:
             self._jogadorDaVez = 'Computador'
         else:
             self._jogadorDaVez = self._jogador
 
-        if modo == 'facil':
-            self._profundidade = 2
-        elif modo == 'normal':
-            self._profundidade = 3
-        else:
-            self._profundidade = 5
+        os.system(['clear', 'cls'][os.name == 'nt'])
+        print("Bem-vindo ao jogo {}! Seu objetivo é formar 4 em linha, coluna ou diagonal antes da IA. ".format(self.__class__.__name__))
 
-    def _atualizaJogada(self):
+    def _alteraJogadorDaVez(self):
         if self._jogadorDaVez == 'Computador':
             self._jogadorDaVez = self._jogador
         else:
@@ -38,7 +37,8 @@ class Ligue4(object):
         while True:
             print 'Jogador da vez:', self._jogadorDaVez
             if self._jogadorDaVez == self._jogador:
-                input_msg = 'Em que posicao você deseja jogar? ' + str(self._posicoesDisponiveis) + '\n'
+                input_msg = 'Em que posicao você deseja jogar? ' + \
+                    str(self._posicoesDisponiveis) + '\n'
 
                 Utils.printEstadoTabuleiro(self._tabuleiro)
 
@@ -52,18 +52,20 @@ class Ligue4(object):
                     jogada = None
                     continue
                 # Acabou jogada, passa vez
-                self._atualizaJogada()
+                self._alteraJogadorDaVez()
             else:
-                # call algoritmo Minimax, imprimir tabuleiro antes ou depois da jogada?
+                # call algoritmo Minimax, imprimir tabuleiro antes ou depois da
+                # jogada?
                 Utils.printEstadoTabuleiro(self._tabuleiro)
                 # Acabou jogada, passa vez
-                self._atualizaJogada()
+                self._alteraJogadorDaVez()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Emula o jogo Ligue4 (Jogador vs Computador). Foi utilizado o algoritmo Minimax com poda alfa e beta para implementação das jogadas do computador.')
     parser.add_argument('nome', type=str, help="Nome do jogador que disputará contra a IA.")
-    parser.add_argument('-modo', type=str, choices=list(('facil', 'normal', 'dificil')), help="Um modo de dificuldade do jogo.")
+    # parser.add_argument('-modo', type=str, choices=list(('facil', 'normal', 'dificil')), help="Um modo de dificuldade do jogo.")
 
     args = parser.parse_args()
 
-    Ligue4(args.modo, args.nome).run()
+    # Ligue4(args.modo, args.nome).run()
+    Ligue4(args.nome).run()
