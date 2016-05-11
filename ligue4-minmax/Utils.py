774c8c551dab7229fccf2f2257d6d6ab0c8b9455
Rodolfo Lottin <rodolfolottin1@gmail.com>
2016-05-11 16:20:11 -0300

    Corrige impressão colunas tabuleiro

--- ligue4-minmax/Utils.py
+++ ligue4-minmax/Utils.py
@@ -8,12 +8,12 @@ class Utils(object):
         tabsFormat = '\t \t \t \t \t \t \t \t \t \t'
 
         for i in xrange(len(tab)):
-            print tabsFormat, '-------------------------------------------------'
-            print tabsFormat, '|', tab[i][0], '|', tab[i][1], '|', tab[i][2], '|', tab[i][3], '|', tab[i][4], '|', tab[i][5], '|'
-        print tabsFormat, '-------------------------------------------------'
+            print tabsFormat, '---------------------------------------------------------'
+            print tabsFormat, '|', tab[i][0], '|', tab[i][1], '|', tab[i][2], '|', tab[i][3], '|', tab[i][4], '|', tab[i][5], '|', tab[i][6], '|'
+        print tabsFormat, '---------------------------------------------------------'
 
     @staticmethod
     # ToDo
     def descobrePosicoesDisponiveisTabuleiro(tab):
-        posicoesDisponiveis = [50]
+        posicoesDisponiveis = []
         return posicoesDisponiveis
