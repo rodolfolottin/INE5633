7216d6e668bf8aac29edb9ddc56645d938787aa0
Rodolfo Lottin <rodolfolottin1@gmail.com>
2016-05-11 17:03:12 -0300

    Corrige algoritmo alphabeta_miniMax

--- ligue4-minmax/Minimax.py
+++ ligue4-minmax/Minimax.py
@@ -4,4 +4,27 @@ class Minimax(object):
         self.profundidade = profundidade
 
     def alphabeta_miniMax(self, nodo, profundidade, alpha, beta, maximizandoJogador):
-    	pass
+        melhorValor = None
+
+        if nodo._isNodoFolha:
+            melhorValor = nodo
+
+        elif maximizandoJogador:
+            melhorValor = alpha
+
+            for filho in nodo._filhos:
+                valorFilho = self.alphabeta_miniMax(filho, profundidade - 1, melhorValor, beta, False)
+                melhorValor = max(melhorValor, valorFilho)
+                if beta <= melhorValor:
+                    break
+
+        else:
+            melhorValor = beta
+
+            for filho in nodo._filhos:
+                valorFilho = self.alphabeta_miniMax(filho, profundidade - 1, alpha, melhorValor, True)
+                melhorValor = min(melhorValor, valorFilho)
+                if melhorValor <= alpha:
+                    break
+
+        return melhorValor
