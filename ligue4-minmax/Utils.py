0199057bb0ef14051d6f245c5031d915afc52bbc
Rodolfo Lottin <rodolfolottin1@gmail.com>
2016-05-12 15:57:48 -0300

    Retira função de gerar posicões Utils

--- ligue4-minmax/Utils.py
+++ ligue4-minmax/Utils.py
@@ -11,9 +11,3 @@ class Utils(object):
             print tabsFormat, '---------------------------------------------------------'
             print tabsFormat, '|', tab[i][0], '|', tab[i][1], '|', tab[i][2], '|', tab[i][3], '|', tab[i][4], '|', tab[i][5], '|', tab[i][6], '|'
         print tabsFormat, '---------------------------------------------------------'
-
-    @staticmethod
-    # ToDo
-    def descobrePosicoesDisponiveisTabuleiro(tab):
-        posicoesDisponiveis = [51, 52, 53, 54, 55, 56]
-        return posicoesDisponiveis
