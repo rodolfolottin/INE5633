8229d2aecf8abaf2720709185659923506b2a01d
Rodolfo Lottin <rodolfolottin1@gmail.com>
2016-05-12 15:57:27 -0300

    Retira atributos desnecessários Nodo

--- ligue4-minmax/Nodo.py
+++ ligue4-minmax/Nodo.py
@@ -3,20 +3,15 @@
 
 class Nodo(object):
 
-    """docstring for Nodo"""
-
-    def __init__(self, repre, index, heuristica, utilidade):
-        self._repre = repre
+    def __init__(self, index, heuristica, utilidade):
         self._index = index
         self._heuristica = heuristica
         self._utilidade = utilidade
         self._isNodoFolha = False
-        self._filhos = []
 
     def __repr__(self):
         return '{}: {} {} {} {} {} {}'.format(self.__class__.__name__,
                                               'Índice de substituição:', self._index,
-                                              'Repr:', self._repre,
                                               'Heurística:', self._heuristica,
                                               'Utilidade:', self._utilidade,
                                               'isNodoFolha:', self._isNodoFolha)
