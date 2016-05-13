edb65b26e352b90232a192486469f2ea4ea065d5
Rodolfo Lottin <rodolfolottin1@gmail.com>
2016-05-13 15:16:00 -0300

    Implementa parserJogada em Utils

--- ligue4-minmax/Utils.py
+++ ligue4-minmax/Utils.py
@@ -12,3 +12,14 @@ class Utils(object):
             print tabsFormat, '---------------------------------------------------------'
             print tabsFormat, '|', Peca.toStr(tab[i][0]), '|', Peca.toStr(tab[i][1]), '|', Peca.toStr(tab[i][2]), '|', Peca.toStr(tab[i][3]), '|', Peca.toStr(tab[i][4]), '|', Peca.toStr(tab[i][5]), '|', Peca.toStr(tab[i][6]), '|'
         print tabsFormat, '---------------------------------------------------------'
+
+    @staticmethod
+    def parserJogada(indiceJogada):
+        if len(indiceJogada) == 2:
+            linha = int(indiceJogada[0])
+            coluna = int(indiceJogada[1])
+        else:
+            # problema com 01, 02, 03...
+            linha = int(0)
+            coluna = int(indiceJogada)
+        return linha, coluna
