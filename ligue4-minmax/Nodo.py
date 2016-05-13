1fefc3752bf5b7532713eec22ff7d0b0ca9198c2
Rodolfo Lottin <rodolfolottin1@gmail.com>
2016-05-13 15:15:36 -0300

    Add board e altera params init Nodo.py

--- ligue4-minmax/Nodo.py
+++ ligue4-minmax/Nodo.py
@@ -3,11 +3,12 @@
 
 class Nodo(object):
 
-    def __init__(self, index, heuristica, utilidade):
+    def __init__(self, index, board, heuristica, utilidade, isNodoFolha):
         self._index = index
+        self._board = board
         self._heuristica = heuristica
         self._utilidade = utilidade
-        self._isNodoFolha = False
+        self._isNodoFolha = isNodoFolha
 
     def __repr__(self):
         return '{}: {} {} {} {} {} {}'.format(self.__class__.__name__,
