6a0388f98b14d82e32029897e8035f77f212ae2a
rodolfolottin <rodolfolottin1@gmail.com>
2016-05-15 19:36:10 -0300

    Altera init Nodo: profundidade

--- ligue4-minmax/Nodo.py
+++ ligue4-minmax/Nodo.py
@@ -3,16 +3,21 @@
 
 class Nodo(object):
 
-    def __init__(self, index, board, heuristica, utilidade, isNodoFolha):
+    def __init__(self, index, board, heuristica, utilidade, profundidade, isNodoFolha):
         self._index = index
         self._board = board
         self._heuristica = heuristica
         self._utilidade = utilidade
+        self._profundidade = profundidade
         self._isNodoFolha = isNodoFolha
 
     def __repr__(self):
-        return '{}: {} {} {} {} {} {}'.format(self.__class__.__name__,
-                                              'Índice de substituição:', self._index,
-                                              'Heurística:', self._heuristica,
-                                              'Utilidade:', self._utilidade,
-                                              'isNodoFolha:', self._isNodoFolha)
+        return '{}: {} {} {} {} {} {} {}'.format(self.__class__.__name__,
+                                                 'Índice de substituição:', self._index,
+                                                 'Heurística:', self._heuristica,
+                                                 'Utilidade:', self._utilidade,
+                                                 'Profundidade', self._profundidade,
+                                                 'isNodoFolha:', self._isNodoFolha)
+
+    def retornPontuacaoTotal(self):
+        return self._heuristica + self._utilidade
