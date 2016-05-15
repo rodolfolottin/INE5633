134ff8c8284905548ef1f155d4c83b893cf7a2e6
rodolfolottin <rodolfolottin1@gmail.com>
2016-05-14 22:50:45 -0300

    Add impressão valores i, j printEstadoTab

--- ligue4-minmax/Utils.py
+++ ligue4-minmax/Utils.py
@@ -7,10 +7,12 @@ class Utils(object):
     @staticmethod
     def printEstadoTabuleiro(tab):
         tabsFormat = '\t \t \t \t \t \t \t \t \t  '
+        tabsFormatRow = '\t \t \t \t \t \t \t \t \t '
 
+        print tabsFormat, '    0       1       2       3       4       5       6'
         for i in xrange(len(tab)):
             print tabsFormat, '---------------------------------------------------------'
-            print tabsFormat, '|', Peca.toStr(tab[i][0]), '|', Peca.toStr(tab[i][1]), '|', Peca.toStr(tab[i][2]), '|', Peca.toStr(tab[i][3]), '|', Peca.toStr(tab[i][4]), '|', Peca.toStr(tab[i][5]), '|', Peca.toStr(tab[i][6]), '|'
+            print tabsFormatRow + str(i), '|', Peca.toStr(tab[i][0]), '|', Peca.toStr(tab[i][1]), '|', Peca.toStr(tab[i][2]), '|', Peca.toStr(tab[i][3]), '|', Peca.toStr(tab[i][4]), '|', Peca.toStr(tab[i][5]), '|', Peca.toStr(tab[i][6]), '|'
         print tabsFormat, '---------------------------------------------------------'
 
     @staticmethod
