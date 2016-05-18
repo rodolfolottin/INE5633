6a1df5269921614c2b352915c8f8a2b6264a4c4d
rodolfolottin <rodolfolottin1@gmail.com>
2016-05-18 01:38:19 -0300

    Add comput e calcul Heuristicas. Declarações funções

--- ligue4-minmax/Heuristica.py
+++ ligue4-minmax/Heuristica.py
@@ -1,4 +1,7 @@
 # coding: utf-8
+from Peca import Peca
+from Nodo import Nodo
+
 class Heuristica(object):
 
     def __init__(self):
@@ -14,6 +17,50 @@ class Heuristica(object):
                 topos.append((linha, coluna))
 
         return topos
+
+    def computarHeuristicaTabuleiro(self, nodo, possiveisMovimentos):
+        pecasAnalisaveis = self.retornaToposTabuleiro(possiveisMovimentos)
+        pontJogador = 0
+        pontComputador = 0
+
+        if nodo._isNodoFolha:
+            if nodo._peca == Peca.COMPUTADOR:
+                nodo._heuristica = 10 ** 5 * profundidade / 2
+            else:
+                nodo._heuristica = - 10 ** 5 * profundidade / 2
+        else:
+            for peca in pecasAnalisaveis:
+                pontComp, pontJog = self.calcularHeuristicas(nodo._board, peca)
+                pontComputador += pontComp
+                pontJogador += pontJog
+
+        return pontComputador * profundidade / 2 - pontJogador * profundidade / 2
+
+    def calcularHeuristicas(self, tabuleiro, peca):
+        listaSequencias = self.retornaSequencias(tabuleiro, peca)
+        pontComp = 0
+        pontJog = 0
+
+        for lista in listaSequencias:
+            if lista[0] == Peca.COMPUTADOR:
+                if lista[1] == 4:
+                    pontComp += 10 ** 5
+                elif lista[1] == 3:
+                    pontComp += 10 ** 3
+                elif lista[1] == 2:
+                    pontComp += 10 ** 2
+            else:
+                if lista[1] == 4:
+                    pontJog += - 10 ** 5
+                elif lista[1] == 3:
+                    pontJog += - 10 ** 3
+                elif lista[1] == 2:
+                    pontJog += - 10 ** 2
+
+        return pontComp, pontJog
+
+    def retornaSequencias(self, tabuleiro, peca):
+        pass
 
     def analisaColunaPecaTopo(self, tabuleiro, peca):
         linha = peca[0]
@@ -38,3 +85,12 @@ class Heuristica(object):
                 break
 
         return listaSequencias
+
+    def analisaLinhaPecaTopo(self, tabuleiro, peca):
+        pass
+
+    def analisaDiagDirPecaTopo(self, tabuleiro, peca):
+        pass
+
+    def analisaDiagEsqPecaTopo(self, tabuleiro, peca):
+        pass
