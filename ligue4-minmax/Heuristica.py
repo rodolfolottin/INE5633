9762228e848ee39ee22fbdb82ff84e74b455e5d3
rodolfolottin <rodolfolottin1@gmail.com>
2016-05-18 00:13:25 -0300

    Add analisaColunaPecaTopo, altera topo para tuplas

--- ligue4-minmax/Heuristica.py
+++ ligue4-minmax/Heuristica.py
@@ -1,7 +1,4 @@
 # coding: utf-8
-from Peca import Peca
-
-
 class Heuristica(object):
 
     def __init__(self):
@@ -13,22 +10,31 @@ class Heuristica(object):
         for num in lista:
             numStr = str(num)
             if max(5, int(numStr[0]) + 1) == 5:
-                topos.append(int(str(int(numStr[0]) + 1) + numStr[1]))
+                linha, coluna = int(numStr[0]) + 1, int(numStr[1])
+                topos.append((linha, coluna))
 
         return topos
 
+    def analisaColunaPecaTopo(self, tabuleiro, peca):
+        linha = peca[0]
+        coluna = peca[1]
+        listaSequencias = []
+        sequencia = 1
 
+        for indiceLinha in xrange(linha + 1, 6, 1):
+            if tabuleiro[indiceLinha - 1][coluna] == tabuleiro[indiceLinha][coluna]:
+                sequencia += 1
+                if sequencia == 4 or max(5, indiceLinha + 1) == 6:
+                    # identificador do jogador
+                    listaSequencias.append(tabuleiro[indiceLinha - 1][coluna])
+                    listaSequencias.append(sequencia)
+                    break
+            else:
+                if sequencia > 1:
+                    listaSequencias.append(tabuleiro[indiceLinha - 1][coluna])
+                    listaSequencias.append(sequencia)
+                else:
+                    sequencia = 0
+                break
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return listaSequencias
