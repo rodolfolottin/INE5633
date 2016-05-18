0488d8ed832f7386894aba6202798db3d7ec8043
rodolfolottin <rodolfolottin1@gmail.com>
2016-05-18 01:02:59 -0300

    Altera init e repr Nodo

--- ligue4-minmax/Nodo.py
+++ ligue4-minmax/Nodo.py
@@ -3,21 +3,19 @@
 
 class Nodo(object):
 
-    def __init__(self, index, board, heuristica, utilidade, profundidade, isNodoFolha):
+    def __init__(self, index, board, peca, heuristica, profundidade, isNodoFolha):
         self._index = index
         self._board = board
+        self._peca = peca
         self._heuristica = heuristica
-        self._utilidade = utilidade
         self._profundidade = profundidade
         self._isNodoFolha = isNodoFolha
 
     def __repr__(self):
-        return '{}: {} {} {} {} {} {} {}'.format(self.__class__.__name__,
-                                                 'Índice de substituição:', self._index,
-                                                 'Heurística:', self._heuristica,
-                                                 'Utilidade:', self._utilidade,
-                                                 'Profundidade', self._profundidade,
-                                                 'isNodoFolha:', self._isNodoFolha)
-
-    def retornPontuacaoTotal(self):
-        return self._heuristica + self._utilidade
+        return '{}: {} {} {} {} {} {} {} {}'.format(self.__class__.__name__,
+                                                    'Índice de substituição:', self._index,
+                                                    'Board:', self._board,
+                                                    'Peca:', self._peca,
+                                                    'Heurística:', self._heuristica,
+                                                    'Profundidade', self._profundidade,
+                                                    'isNodoFolha:', self._isNodoFolha)
