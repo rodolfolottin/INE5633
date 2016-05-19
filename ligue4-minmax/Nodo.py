6c7b914284a8f8b3910dfcf45d424d965544bec1
rodolfolottin <rodolfolottin1@gmail.com>
2016-05-19 20:25:11 -0300

    Adiciona alpha e beta em Nodo

--- ligue4-minmax/Nodo.py
+++ ligue4-minmax/Nodo.py
@@ -3,7 +3,7 @@
 
 class Nodo(object):
 
-    def __init__(self, index, board, peca, heuristica, profundidade, isNodoFolha, caminhoJogadas):
+    def __init__(self, index, board, peca, heuristica, profundidade, isNodoFolha, caminhoJogadas, alpha, beta):
         self._index = index
         self._board = board
         self._peca = peca
@@ -11,12 +11,17 @@ class Nodo(object):
         self._profundidade = profundidade
         self._isNodoFolha = isNodoFolha
         self._caminhoJogadas = caminhoJogadas
+        self._alpha = alpha
+        self._beta = beta
 
-    # def __repr__(self):
-    #     return '{}: {} {} {} {} {} {} {} {}'.format(self.__class__.__name__,
-    #                                                 'Índice de substituição:', self._index,
-    #                                                 'Board:', self._board,
-    #                                                 'Peca:', self._peca,
-    #                                                 'Heurística:', self._heuristica,
-    #                                                 'Profund
-    #                                                 'isNodoFolha:', self._isNodoFolha)
+    def __repr__(self):
+        return '{}: {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} '.format(self.__class__.__name__,
+                                                                                   'Índice de substituição:', self._index,
+                                                                                   'Board:', self._board,
+                                                                                   'Peca:', self._peca,
+                                                                                   'Heurística:', self._heuristica,
+                                                                                   'Profundidade:', self._profundidade,
+                                                                                   'isNodoFolha:', self._isNodoFolha,
+                                                                                   'caminhoJogadas:', self._caminhoJogadas,
+                                                                                   'alpha:', self._alpha,
+                                                                                   'beta:', self._beta)
