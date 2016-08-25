4612972ad42e41143a9ea6d5486cb4fa8222956f
rodolfolottin <rodolfolottin1@gmail.com>
2016-08-25 00:29:20 -0300

    Adiciona implementação utilizada Trabalho 4

---
+++ TravellerSalesman/src/tsp/TSP_GA.java
@@ -0,0 +1,91 @@
+/*
+* TSP_GA.java
+* Create a tour and evolve a solution
+*/
+
+package tsp;
+
+import java.util.Scanner;
+
+public class TSP_GA {
+
+    public static void main(String[] args) {
+
+        // Create and add our cities
+        City city = new City(60, 200);
+        TourManager.addCity(city);
+        City city2 = new City(180, 200);
+        TourManager.addCity(city2);
+        City city3 = new City(80, 180);
+        TourManager.addCity(city3);
+        City city4 = new City(140, 180);
+        TourManager.addCity(city4);
+        City city5 = new City(20, 160);
+        TourManager.addCity(city5);
+        City city6 = new City(100, 160);
+        TourManager.addCity(city6);
+        City city7 = new City(200, 160);
+        TourManager.addCity(city7);
+        City city8 = new City(140, 140);
+        TourManager.addCity(city8);
+        City city9 = new City(40, 120);
+        TourManager.addCity(city9);
+        City city10 = new City(100, 120);
+        TourManager.addCity(city10);
+        City city11 = new City(180, 100);
+        TourManager.addCity(city11);
+        City city12 = new City(60, 80);
+        TourManager.addCity(city12);
+        City city13 = new City(120, 80);
+        TourManager.addCity(city13);
+        City city14 = new City(180, 60);
+        TourManager.addCity(city14);
+        City city15 = new City(20, 40);
+        TourManager.addCity(city15);
+        City city16 = new City(100, 40);
+        TourManager.addCity(city16);
+        City city17 = new City(200, 40);
+        TourManager.addCity(city17);
+        City city18 = new City(20, 20);
+        TourManager.addCity(city18);
+        City city19 = new City(60, 20);
+        TourManager.addCity(city19);
+        City city20 = new City(160, 20);
+        TourManager.addCity(city20);
+
+        boolean tournamentSelection;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("1 - Tournament Selection");
+        System.out.println("2 - Roulette Selection");
+        int algorithm = scanner.nextInt();
+
+        if (algorithm == 1) {
+        	tournamentSelection = true;
+        	System.out.println("Tournament Selection it is!");
+        } else {
+        	tournamentSelection = false;
+        	System.out.println("Roulette Selection it is!");
+        }
+        
+        // Initialize population
+        Population pop = new Population(50, true);
+        int initialDistance = pop.getFittest().getDistance();
+        System.out.println("Initial distance: " + initialDistance);
+
+        // Evolve population for 100 generations
+        pop = GA.evolvePopulation(pop, tournamentSelection);
+        for (int i = 0; i < 100; i++) {
+            pop = GA.evolvePopulation(pop, tournamentSelection);
+        }
+
+        // Print final results
+        System.out.println("Finished");
+        int finalDistance = pop.getFittest().getDistance();
+        System.out.println("Final distance: " + finalDistance);
+        System.out.println("Solution:");
+        System.out.println(pop.getFittest());
+        System.out.println("Dif: " + (initialDistance - finalDistance));
+        
+    }
+}
